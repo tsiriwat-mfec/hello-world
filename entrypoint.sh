9d723083841bad95bbc8f6a2c6024c9418638900
T.Siriwat-MFEC <126653005+tsiriwat-mfec@users.noreply.github.com>
2023-03-28 17:40:53 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
 time=$(date)
-echo "time=$time" >> $GITHUB_OUTPUT
-echo "github.sha" >> ${{ github.sha }}
-echo "GITHUB_SHA" >> $GITHUB_SHA
-echo "GITHUB_SHA" >> $TEST_ENV
+echo "time=$time >>" $GITHUB_OUTPUT
+echo "github.sha >>" ${{ github.sha }}
+echo "GITHUB_SHA >>" $GITHUB_SHA
+echo "TEST_ENV >>" $TEST_ENV
